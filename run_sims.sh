604b30b689ff3f02c6d9d04dd8cabec2ed6f0ca4
Felipe Montefuscolo <felipe.mt87@gmail.com>
2018-06-08 06:59:23 -0400

    add options to set dry_run outside the script

--- run_sims.sh
+++ run_sims.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ###### User preferences
 
-dry_run=false
+dry_run=?false
 outdir=/Users/felipe/bitme/outdir
 input=/Users/felipe/bitme/data/bitmex_1day.csv
 
